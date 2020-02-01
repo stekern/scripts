40a9f9607494e3d14558a043addf18d753795511
stekern <stekern@users.noreply.github.com>
2020-02-01 14:28:47 +0100

    WIP: Use interactive prompt for selecting windows

--- tmux_copy_layout/tmux_copy_layout.sh
+++ tmux_copy_layout/tmux_copy_layout.sh
@@ -18,16 +18,53 @@ verify_active_tmux_session() {
 
 main() {
 	verify_active_tmux_session
+	local TMP_FILE="/tmp/tmux_copy_window_$(date +'%s')"
+	local MAXIMUM_WAIT_IN_SECONDS="5"
+
+	tmux command-prompt -p "Copy layout from window: ","Copy layout to window: " "run -b 'echo %1 %2 > $TMP_FILE'"
+	local num_seconds="0"
+	while [ ! -e "$TMP_FILE" ]; do
+		if [ "$num_seconds" -gt "$MAXIMUM_WAIT_IN_SECONDS" ]; then
+			printf "[x] Waited too long for response. Exiting ...\n"
+			exit 1
+		fi
+		sleep 1
+		num_seconds=$((num_seconds + 1))
+	done
 
 	local first_pane
 	local layout
 	local num_panes
+	local target_panes
+	local from
 	local to
-	layout="$(tmux display-message -p '#{window_layout}')"
-	num_panes="$(tmux display-message -p '#{window_panes}')"
-	tmux new-window
-	to="$(tmux list-windows | tail -1 | sed -r 's/^([0-9]+):.*$/\1/')"
-	for _ in $(seq $((num_panes - 1))); do
+
+	IFS=$' ' read -r from to <<<"$(cat "$TMP_FILE")"
+	# If from window is set
+	if [ -n "$from" ]; then
+		layout="$(tmux display-message -p -t "$from.0" '#{window_layout}')"
+		num_panes="$(tmux display-message -p -t "$from.0" '#{window_panes}')"
+	else
+		# Use layout of current window
+		layout="$(tmux display-message -p '#{window_layout}')"
+		num_panes="$(tmux display-message -p '#{window_panes}')"
+	fi
+	# If to window is not set, create a new window
+	if [ ! -n "$to" ]; then
+		target_panes=1
+		tmux new-window
+	else
+		tmux select-window -t "$to"
+		target_panes="$(tmux display-message -p -t "$to.0" '#{window_panes}')"
+	fi
+
+	if [ "$target_panes" -gt "$num_panes" ]; then
+		printf "[x] Target window has more panes than in the given layout\n"
+		exit 1
+	fi
+
+	#to="$(tmux list-windows | tail -1 | sed -r 's/^([0-9]+):.*$/\1/')"
+	for _ in $(seq $((num_panes - target_panes))); do
 		tmux split-window -t "$to"
 	done
 	first_pane="$(tmux list-panes | head -1 | sed -r 's/^([0-9]+):.*$/\1/')"
